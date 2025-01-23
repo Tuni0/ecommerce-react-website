b161f54a65dccfaf612589a94cf80e4e5e578eca
Tuni0 <wiktor.mazepa@bfsapp.pl>
2025-01-23 01:31:55 +0100

    beta-0.0.1

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center">
+      <h1 className="text-2xl font-bold">E-commerce</h1>
+      <nav>
+        <ul className="flex space-x-4"></ul>
+      </nav>
+    </header>
+  );
+}
